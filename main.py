32e393f74fc587a805dc44538b126115c5bb3b71
rajneesh44 <rajneeshsharma338@gmail.com>
2020-02-17 23:25:17 +0530

    Data: fetched from twitter

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tweepy
 import datetime
 import json 
 
-file_path = '../config/api.json'
+file_path = 'api.json'
 
 with open(file_path) as f:
 	twitter_api = json.loads(f.read())
@@ -18,5 +18,26 @@ auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth)
 
 class StreamListener(tweepy.StreamListener):
+	def on_status(self,status):
+
+		if('RT @' not in status.text):
+			tweet_item={
+			'id_str':status.id_str,
+			'text':status.text,
+			'username':status.user.screen_name,
+			'name':status.user.name,
+			'profile_image_url':status.user.profile_image_url,
+			'received_at':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			}
+			print(tweet_item)
+
+	def on_error(self,status_code):
+		if status_code==420:
+			return False
+
+stream_Listener = StreamListener()
+stream = tweepy.Stream(auth=api.auth, listener = stream_Listener)
+stream.filter(track=["@WarbyParker", "@Bonobos", "@Casper","@Glossier", "@DollarShaveClub", "@Allbirds","pizza"])
+
 	
 
